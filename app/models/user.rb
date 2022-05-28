e87b1bb8cd0541531595257fdce8060727c62ebe
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-28 16:59:49 +0300

    adds user relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  has_many :events
+
+  validates :name, presence: true, length: {maximum: 25 }
+  validates :email, presence: true, length: {maximum: 355 }
+  validates :email, uniqueness: true
+  validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
 end
