c76eea8257ebd92e0e392969f57039a1b5122fc5
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-05-30 22:15:15 +0300

    added devise and changed bootstrap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,5 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "./application.css"
-import "./events.css"
 
 import * as bootstrap from "bootstrap"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,19 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  before_validation :set_name, on: :create
   has_many :events
 
   validates :name, presence: true, length: {maximum: 25 }
   validates :email, presence: true, length: {maximum: 355 }
   validates :email, uniqueness: true
   validates :email, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+
+  private
+
+  def set_name
+    self.name = "Товарисч №#{rand(777)}" if self.name.blank?
+  end
 end
