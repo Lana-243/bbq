234b1c010a7a582f86a7a628c9bc325439016c2a
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-07-18 23:18:16 +0300

    added subscription validation for existing users

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,11 +4,17 @@ class Subscription < ApplicationRecord
 
   validates :event, presence: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
-  validates :user_email, uniqueness: { scope: :event_id }, unless: -> { user.present? }
-  validates :user, uniqueness: { scope: :event_id }, if: -> { user.present? }
-  validate :event_creator_cannot_subscribe
+  with_options if: -> { user.present? } do
+    validates :user, uniqueness: { scope: :event_id }
+    validate :event_creator_cannot_subscribe
+  end
+
+  with_options unless: -> { user.present? } do
+    validates :user_name, presence: true
+    validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
+    validates :user_email, uniqueness: { scope: :event_id }
+    validate :user_is_signed_up
+  end
 
   def user_name
     user&.name || super
@@ -23,6 +29,12 @@ class Subscription < ApplicationRecord
   def event_creator_cannot_subscribe
     if event.user == user
       errors.add(:user, message: I18n.t('shared.errors.models.subscription.event_creator_cannot_subscribe'))
+    end
+  end
+
+  def user_is_signed_up
+    if User.where(email: user_email).present?
+      errors.add(:user_email, message: I18n.t('shared.errors.models.subscription.user_signed_up'))
     end
   end
 end
