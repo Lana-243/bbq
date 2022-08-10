ff9eb754f6efa381a7faa215b1912adfc61d1d50
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-08-10 11:49:57 +0300

    add standard avatar if nil

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,11 @@ module ApplicationHelper
   end
 
   def user_avatar(user)
-    asset_path('user.png')
+    if user.avatar.present?
+      user.avatar.variant(:mini)
+    else
+      asset_path('user.png')
+    end
   end
 
   def fa_icon(icon_class)
