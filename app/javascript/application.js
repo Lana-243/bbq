89c4f48f69915ad80f06b59eb89a65d1462154a0
Lana-243 <84347249+Lana-243@users.noreply.github.com>
2022-08-24 11:12:54 +0300

    added lightbox2 extra code

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,6 +2,7 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
-import './map'
 import "jquery"
-import "lightbox2"
+import './map'
+import './photos.js'
+// import "lightbox2"
---
+++ app/javascript/photos.js
@@ -0,0 +1,17 @@
+import lightbox from 'lightbox2'
+
+document.addEventListener('turbo:load', () => {
+  lightbox.init()
+
+  lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'fadeDuration': 200,
+    'imageFadeDuration': 200,
+    'fitImagesInViewport': true
+  })
+})
+
+window.addEventListener("popstate", function (event) {
+  location.reload(true);
+})
